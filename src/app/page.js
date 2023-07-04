098b7dac06717fe72e39c110f60798211cba7fe8
John Doe <tirunaharipandu@gmail.com>
2023-07-04 10:40:24 +0530

    resume download option added

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,10 @@ export default function Home() {
     <div className= "flex items-center gap-[50px] p-12 sm:pl-[10%] sm:pb-[20%] verflow-hidden pb-[90%] ">
     <div className=" flex flex-col gap-12 ">
       <h1 className="text-6xl bg-gradient-to-b from-green-700 to-blue-500 text-transparent bg-clip-text">
-        Namaste, I am Kruthardh
+        Namaste, I am Kruthardh,
       </h1>
       <p className="text-lg font-light">
-        asdfasdf
-      {/* I am a enthusiastic full stack developer with deep love for football. */}
+      a enthusiastic full stack developer with deep love for football. 
       </p>
       <a href="/about" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
 <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-500 group-hover:translate-x-0 ease">
@@ -20,7 +19,9 @@ export default function Home() {
 <span class="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">About Me</span>
 <span class="relative invisible">About Me</span>
 </a>
-
+<a href="https://drive.google.com/file/d/1d2yshjk-ZYmiJk3TFzJkEJwHHAHFCSRS/view?usp=drive_link" className="text-cyan-400 hover:text-green-400" download>
+        Download my Resume
+      </a>
     </div>
     <div className="w-full h-45 object-contain animate-pulse ml-40 ">
       <Image src={frontend} alt="" className=""/>
